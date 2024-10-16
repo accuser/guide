d3b39046546a4addc5e108567eadb3260dedc239
Matthew Gibbons <mhgibbons@me.com>
2024-10-16 14:55:02 +0000

    Refactor code to handle 404 errors and improve error page layout

--- src/routes/[...slug]/+page.server.ts
+++ src/routes/[...slug]/+page.server.ts
@@ -1,4 +1,5 @@
 import { astFromMarkdown } from '$lib/ast/ast-from-markdown';
+import { error } from '@sveltejs/kit';
 import { readFile } from 'node:fs/promises';
 import type { PageServerLoad } from './$types';
 
@@ -10,9 +11,13 @@ const allowed = (value: string) => Boolean(value) && value.startsWith('.') === f
 export const load = (async ({ params: { slug } }) => {
 	const path = slug.split('/').filter(allowed).join('/');
 
-	const src = await Promise.any(
-		['.md', '/index.md'].map((suffix) => readFile(`docs/${path}${suffix}`, 'utf-8'))
-	);
+	try {
+		const src = await Promise.any(
+			['.md', '/index.md'].map((suffix) => readFile(`docs/${path}${suffix}`, 'utf-8'))
+		);
 
-	return { ast: astFromMarkdown(src) };
+		return { ast: astFromMarkdown(src) };
+	} catch {
+		throw error(404, 'Not found');
+	}
 }) satisfies PageServerLoad;
