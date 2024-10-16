2ae41f19150380fbdb71e1cfd80e7eff53039240
Matthew Gibbons <mhgibbons@me.com>
2024-10-16 16:05:00 +0000

    Refactor directives to include Kbd component

--- src/lib/defaults/directives.ts
+++ src/lib/defaults/directives.ts
@@ -1,12 +1,8 @@
+import Kbd from '$lib/directives/Kbd.svelte';
 import Toc from '$lib/directives/Toc.svelte';
-import type { Directives } from '$lib/types/directives';
-import type { LeafDirective } from 'mdast-util-directive';
 
-interface DirectiveMap {
-	toc: LeafDirective;
-}
-
-const directives: Directives<DirectiveMap[keyof DirectiveMap]> = {
+const directives = {
+	kbd: Kbd,
 	toc: Toc
 };
 
---
+++ src/lib/directives/Kbd.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Node from '$lib/components/Node.svelte';
+	import type { TextDirective } from 'mdast-util-directive';
+
+	let { children }: TextDirective = $props();
+</script>
+
+<kbd
+	>{#each children as node}<Node {...node} />{/each}</kbd
+>
+
+<style lang="postcss">
+</style>
