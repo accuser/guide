e83f68428c68ba9fe9f48e152b754010dc5ec445
Matthew Gibbons <mhgibbons@me.com>
2024-10-17 12:36:49 +0000

    Better code

--- src/lib/components/Mdast/Code.svelte
+++ src/lib/components/Mdast/Code.svelte
@@ -2,10 +2,23 @@
 	import { astFromCode } from '$lib/ast/ast-from-code';
 	import Hast from '$lib/components/Hast.svelte';
 	import type { Code } from 'mdast';
+	import CopyButton from '../CopyButton.svelte';
 
 	let { lang, meta, value }: Code = $props();
 </script>
 
 <pre><code {lang} data-meta={meta}
 		>{#await astFromCode(value, lang)}{value}{:then ast}<Hast {ast} />{:catch}{value}{/await}</code
-	></pre>
+	><CopyButton {value} />
+</pre>
+
+<style lang="postcss">
+	pre {
+		@apply relative;
+		@apply overflow-x-auto;
+	}
+
+	code {
+		@apply block;
+	}
+</style>
