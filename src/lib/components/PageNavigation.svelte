d15f6995860491d6c84e3b9e68b7105fd5132cb7
Matthew Gibbons <mhgibbons@me.com>
2024-10-19 13:03:09 +0000

    Refactor Navigation, PageNavigation, and SocialLink components

---
+++ src/lib/components/PageNavigation.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	let pathname = usePathname();
+	let allPages = navigation.flatMap((group) => group.links);
+	let currentPageIndex = allPages.findIndex((page) => page.href === pathname);
+
+	if (currentPageIndex === -1) {
+		return null;
+	}
+
+	let previousPage = allPages[currentPageIndex - 1];
+	let nextPage = allPages[currentPageIndex + 1];
+
+	if (!previousPage && !nextPage) {
+		return null;
+	}
+</script>
+
+<div class="flex">
+	{#if previousPage}
+		<div class="flex flex-col items-start gap-3">
+			<PageLink label="Previous" page={previousPage} previous />
+		</div>
+	{/if}
+	{#if nextPage}
+		<div class="ml-auto flex flex-col items-end gap-3">
+			<PageLink label="Next" page={nextPage} />
+		</div>
+	{/if}
+</div>
---
+++ src/lib/components/SocialLink.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Component, Snippet } from 'svelte';
+
+	let {
+		href,
+		icon: Icon,
+		children
+	}: { href: string; icon: Component; children: Snippet } = $props();
+</script>
+
+<a {href} class="group">
+	<span class="sr-only">{@render children?.()}</span>
+	<Icon
+		class="h-5 w-5 fill-zinc-700 transition group-hover:fill-zinc-900 dark:group-hover:fill-zinc-500"
+	/>
+</a>
